398adbf287812906fb549bbac5149eb46bce9419
evanBergeron <bergeronej@gmail.com>
2015-03-23 23:56:52 -0500

    Cleaned up files

--- expressionize.py
+++ expressionize.py
@@ -1,103 +1,99 @@
-import ast
 import sys
+import ast
 import unparse
 from cStringIO import StringIO
 
-class Expressionizer(ast.NodeTransformer):
+# Imports sys and defines a new print function
+IMPORT_SYS = "__import__('sys')._getframe(-1).f_locals.update({'sys':__import__('sys')}) or sys._getframe(-1).f_locals.update({'printf': lambda *s : sys.stdout.write('%s\\n' % ' '.join(map(str, s)))})\n"
+
+class OrBody(ast.NodeTransformer):
+    def generic_visit(self, node):
+        if 'body' in node.__dict__:
+            # node.__dict__['body'] = self.orTogether(node)
+            return self.orTogether(node)
+        return node
+
+    # @staticmethod
+    def orTogether(self, node):
+        goal = ['False']
+        tmp = StringIO()
+        for subnode in node.__dict__['body']:
+            goal.append(self.unparsed(subnode).strip())
+        goal = " or ".join(goal)
+        # assert(False)
+        return self.parsed(goal)
+
+    @staticmethod
+    def unparsed(node):
+        tmp = StringIO()
+        unparse.Unparser(node, tmp)
+        result = tmp.getvalue()
+        tmp.close()
+        return result
+
+    @staticmethod
+    def parsed(source):
+        source = ast.parse(source)
+        return source.__dict__['body'][0]
+
+class ExpressionizeAssignmentsAndPrint(ast.NodeTransformer):
 
     def visit_Print(self, node):
-        '''
-        print someObject -->
-            __import__('sys').stdout.write(str(someObject))
-        '''
         printedObjects = node.__dict__['values']
-        print printedObjects
         tmp = StringIO()
         for obj in printedObjects:
             unparse.Unparser(obj, tmp)
-        print tmp.getvalue()
-
-        goal = "__import__('sys').stdout.write(%%s)"
-        newNode = self.parsed(goal)
-        # return newNode
-        # if len(printedObjects) == 0:
-        #     # For now, erase empty print statements
-        #     return None
-        # printedObject = printedObjects[0]
-        # # sys.stdout.write expects a character buf arg
-        # printExpr = "__import__('sys').stdout.write(str('foo'))"
-        # printExprNode = ast.parse(printExpr)
-        # # Sneak into printExpr's stdou.write argument and change it
-        # printExprNode.__dict__['body'][0].__dict__['value'].__dict__['args'][0].__dict__['args'][0] = printedObject
-        # return printExprNode
-
-    @staticmethod
-    def assign_print_info(node):
-        print "##########################"
-        print "# Visiting an assignment #"
-        print "##########################"
-        print node.__dict__
-        print
-        print "Targets: (or variables)"
-        for target in node.__dict__['targets']:
-            print target.__dict__
-        print "Value:"
-        print node.__dict__['value'].__dict__
-        print "##########################"
+        goal = "printf(%s)" % str(tmp.getvalue())
+        tmp.close()
+        return self.parsed(goal)
+        return node
 
     def visit_Assign(self, node):
+        # Unparse node into currentLineOfCode
         tmp = StringIO()
         unparse.Unparser(node, tmp)
         currentLineOfCode = tmp.getvalue()
         tmp.write("")
         tmp.close()
-        currentLineOfCode = currentLineOfCode.split("=")
-        (target, value) = currentLineOfCode[0].strip(), currentLineOfCode[1].strip()
-        # Note that this eval hack will not work for certain python values
-        # eg - exceptions
-        d = {target:eval(value)}
-        goal = "__import__('sys')._getframe(-1).f_locals.update(%s)\n" % d
-        goal = self.parsed(goal)
-        return goal
-
-    def visit_Raise(self, node):
-        # All exceptions are FutureWarnings. Proof of concept,
-        return self.parsed("raise FutureWarning")
-
-    @classmethod
-    def print_all(self, node):
-        for subnode in ast.walk(node):
-            if 'body' in subnode.__dict__:
-                print subnode
-                print subnode.__dict__
-
-    @staticmethod
-    def printDict(node):
-        print node.__dict__
+        # Grab target and value
+        target = currentLineOfCode.split("=")[0]
+        value = currentLineOfCode.split("=")[1]
+        # List comphrensions have side effects
+        goal = "([%s for %s in [%s]] and False)" % (target, target, value)
+        return self.parsed(goal)
 
     @staticmethod
     def parsed(source):
-        # Returns the ast object corresponding to a single line of code.
-        # (Removes the module cruft)
         source = ast.parse(source)
-        result = source.__dict__['body'][0]
-        # print result
-        return result
+        return source.__dict__['body'][0]
 
-def parsed(source):
-    # Returns the ast object corresponding to a single line of code.
-    # (Removes the module cruft)
-    source = ast.parse(source)
-    result = source.__dict__['body'][0]
-    # print result
-    return result
+def orTogether(src):
+    src = src.split("\n")
+    return "False" + " or ".join(src)
 
-with open(sys.argv[1], 'r') as sourceCode:
-    sourceCode = sourceCode.read()
-    unmodified = ast.parse(sourceCode)
-    transformer = Expressionizer()
-    # transformer.print_all(unmodified)
-    modified = transformer.visit(unmodified)
-    result = StringIO()
-    unparse.Unparser(modified, result)
-    print "%s\n" % result.getvalue()
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        source = sys.argv[1]
+    else:
+        print "No command line argument found"
+        exit(-1)
+    with open(source, "r+") as src:
+        src = src.read()
+
+        src = IMPORT_SYS + src
+        unmodified = ast.parse(src)
+
+        transformer = ExpressionizeAssignmentsAndPrint()
+        modified = transformer.visit(unmodified)
+
+        result = StringIO()
+
+        orMaster = OrBody()
+        modified = orMaster.visit(modified)
+
+        unparse.Unparser(modified, result)
+
+
+        print "%s\n" % result.getvalue().strip()
+        result.close()
+        # print orTogether(result.getvalue())
