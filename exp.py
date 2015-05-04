503d817031a88b95087e63fdddd2688a1f3a9557
evanBergeron <bergeronej@gmail.com>
2015-05-04 18:19:42 -0500

    Got nesting to work, todo includes body to list comphrension and namespace issues

--- exp.py
+++ exp.py
@@ -18,14 +18,14 @@ DEBUG = False
 
 def parsed(source):
     source = ast.parse(source)
-    return source.__dict__['body'][0]
+    return source.body[0]
 
 def unparsed(node):
     tmp = StringIO()
     unparse.Unparser(node, tmp)
     result = tmp.getvalue()
     tmp.close()
-    return result
+    return result.strip()
 
 def dfs_fix_children(node):
     for name, field in ast.iter_fields(node):
@@ -40,17 +40,14 @@ def dfs_fix_children(node):
     return None
 
 def visit_Module(node):
-    dfs_fix_children(node)
     return node
 
 def visit_Print(node):
-    dfs_fix_children(node)
     args = ", ".join([unparsed(value) for value in node.values])
     goal = "printf(%s)" % args
     return parsed(goal)
 
 def visit_Assign(node):
-    dfs_fix_children(node)
     targets = unparsed(node.targets)
     value = unparsed(node.value)
     goal = "[%s for %s in [%s]]" % (targets, targets, value)
@@ -59,17 +56,48 @@ def visit_Assign(node):
 def visit_Num(node):
     return node
 
+def visit_FunctionDef(node):
+    name = node.name
+    args = unparsed(node.args)
+    body = unparsed(node.body)
+    func = "lambda %s : %s" % (args, body)
+    goal = "[%s for %s in [%s]]" % (name, name, func)
+    return parsed(goal)
+
+def visit_Return(node):
+    return node.value
+
 options = {
 
-    ast.Module : visit_Module,
-    ast.Print  : visit_Print,
-    ast.Num    : visit_Num,
-    ast.Assign : visit_Assign,
+    ast.Module      : visit_Module,
+    ast.Print       : visit_Print,
+    ast.Num         : visit_Num,
+    ast.Assign      : visit_Assign,
+    ast.Num         : visit_Num,
+    ast.FunctionDef : visit_FunctionDef,
+    ast.Return      : visit_Return,
 
 }
 
 def expressionize(node):
     if DEBUG: print type(node)
+    dfs_fix_children(node)
+    if hasattr(node, 'body') and type(node) is not ast.Module:
+        # Replace multiple lines with a single list comphrension
+        # TODO - not fully working
+        if type(node.body) is list and len(node.body) > 1:
+            lines = []
+            for item in node.body:
+                lmbda = ast.Lambda(args = [], body = item)
+                lines.append(lmbda)
+                print lines
+            goal =  str([unparsed(line).replace("\n", "") for line in lines]).replace("'", "")
+            print goal
+            print
+            node.body = parsed("[_() for _ in %s]" % goal)
+        elif isinstance(node.body, ast.AST):
+            pass
+
     try: return options[type(node)](node)
     except KeyError:
         return node
